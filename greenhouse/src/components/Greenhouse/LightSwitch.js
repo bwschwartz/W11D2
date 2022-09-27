a327bfb1a314835942bcae451c01247f4e84ec2d
Brian <brianrlam@gmail.com>
2022-09-27 15:14:23 -0400

    allow theme change

---
+++ greenhouse/src/components/Greenhouse/LightSwitch.js
@@ -0,0 +1,17 @@
+import './LightSwitch.css';
+
+function LightSwitch() {
+  function toggle() {
+    
+  }
+
+
+  return (
+    <div className="light-switch day">
+      <div className="on">DAY</div>
+      <div className="off">NIGHT</div>
+    </div>
+  );
+}
+
+export default LightSwitch;
